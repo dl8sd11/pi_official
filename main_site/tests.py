c47dd6937442dc1d41193c21add76ec6b15981f5
dl8sd11 <tmd910607@gmail.com>
2019-05-26 19:56:04 +0800

    burnside testcases

--- main_site/tests.py
+++ main_site/tests.py
@@ -1,9 +1,10 @@
 from django.test import TestCase
 from django.urls import reverse
 from .models import Question
+from .burn_side import BurnSide
 
 def create_question(asker="Tester",title="test",cat="Meow",content="Not a real question"):
-    reply = Question(asker="Tester",cat="Meow",content="test")
+    reply = Question(asker="Tester",cat="Meow",content="test",order=1)
     reply.submit()
     return reply
 
@@ -48,3 +49,46 @@ class ResponseQuestionViewTests(TestCase):
         query_question = create_question()
         response = self.client.get(reverse('response_questions',args=[query_question.id]))
         self.assertEqual(response.context['question'],query_question)
+
+class BurnSideTests(TestCase):
+    def test_prime_n(self):
+        """
+        n is prime
+        """
+        agent = BurnSide(7,3)
+        self.assertIn("198.0000000000",agent.solve())
+    
+    def test_composite_n(self):
+        """
+        n is composite
+        """
+        agent = BurnSide(6,6)
+        self.assertIn("4291.0000000000",agent.solve())
+    
+    def test_neg_n(self):
+        """
+        n is neg
+        """
+        agent = BurnSide(-8,4)
+        self.assertIn("-0.0656747818",agent.solve())
+
+    def test_zero_n(self):
+        """
+        n is zero
+        """
+        agent = BurnSide(0,4)
+        self.assertIn("0.0000000000",agent.solve())
+        
+    def test_zero_k(self):
+        """
+        k is zero
+        """
+        agent = BurnSide(5,0)
+        self.assertIn("0.0000000000",agent.solve())
+    
+    def test_neg_k(self):
+        """
+        k is neg
+        """
+        agent = BurnSide(12,-4)
+        self.assertIn("696166.0000000000",agent.solve())
